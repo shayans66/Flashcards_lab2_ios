10224a6b34e09c42033a7a995728a01372975953
shayan <42shayan42@gmail.com>
2021-03-13 00:58:31 -0800

    disappearing button

--- mpd_lab2/ViewController.swift
+++ mpd_lab2/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var frontLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapOnFlashCard(_ sender: Any) {
+        frontLabel.isHidden = true;
+    }
+    
+        
 }
 
