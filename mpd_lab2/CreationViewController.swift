326bee592df3cb6c77f92af9323624bd78698ff2
shayan <42shayan42@gmail.com>
2021-03-13 00:28:11 -0800

    custom createviewcontroller

---
+++ mpd_lab2/CreationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreationViewController.swift
+//  mpd_lab2
+//
+//  Created by Shayan Sarnevesht on 3/13/21.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
